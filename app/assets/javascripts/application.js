2e08c7d4e7554376c1df541a9336f3684c46589a
activeliang <activeliang@gmail.com>
2017-08-31 00:54:20 +0800

    remove a js to vender

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require bootstrap-datepicker/core
 //= require bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN
 //= require pielabel
+//= require chartsjs-plugin-data-labels
 //= require_tree .
 
 
